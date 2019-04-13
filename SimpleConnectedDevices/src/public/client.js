4d93f11e78c6a65f2eab671fd9e2140178b73080
AlemayehuMekonen <47207378+AlemayehuMekonen@users.noreply.github.com>
2019-04-13 13:07:10 +0400

    Update client.js

--- SimpleConnectedDevices/src/public/client.js
+++ SimpleConnectedDevices/src/public/client.js
@@ -1,7 +1,57 @@
-function ledON() {
-    console.log ("sending a message to turn on the LED");
+/* 
+This is the websocket library which will allow us to send messages
+back to the web server 
+*/
+var socket = io();
+
+socket.on('event', (data) => {
+	console.log('received data from webserver: ' + data);
+});
+
+socket.on('buttonPressed', (data) => {
+	console.log('received button pressed event from webserver: ' + data);
+	displayTheImage();
+});
+
+socket.on('buttonReleased', (data) => {
+	console.log('received button released from webserver: ' + data);
+	hideTheImage();
+});
+
+function displayTheImage () {
+	var placeholder = document.getElementById("placeholder");
+
+	placeholder.src = "randomImage.jpeg"; // put images in public folder
+
+	placeholder.style.opacity = 100;
 }
 
-function ledOFF() {
-    console.log ("sending a message to turn off the LED");
+function hideTheImage () {
+	var placeholder = document.getElementById("placeholder");
+
+	placeholder.style.opacity = 0;
+}
+
+function redLEDOn() {
+  socket.emit('redLEDOn'); 
+}
+
+function redLEDOff() {
+  socket.emit('redLEDOff');
+}
+
+function greenLEDOn() {
+  socket.emit('greenLEDOn'); 
+}
+
+function greenLEDOff() {
+  socket.emit('greenLEDOff');
+}
+
+function blueLEDOn() {
+  socket.emit('blueLEDOn'); 
+}
+
+function blueLEDOff() {
+  socket.emit('blueLEDOff');
 }
