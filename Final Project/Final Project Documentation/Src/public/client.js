088433d7d7a8a770eaa24653725d63baf6fde0b0
AlemayehuMekonen <47207378+AlemayehuMekonen@users.noreply.github.com>
2019-05-17 19:15:04 +0400

    Update client.js

--- Final Project/Final Project Documentation/Src/public/client.js
+++ Final Project/Final Project Documentation/Src/public/client.js
@@ -1,1 +1,21 @@
 
+/* 
+This is the websocket library which will allow us to send messages
+back to the web server 
+Alemayehu Abebe
+*/
+var socket = io();
+
+
+
+function redLEDOn() {
+  socket.emit('redLEDOn'); 
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+
+function redLEDOff() {
+  socket.emit('redLEDOff');
+  audio.pause();
+}
+
